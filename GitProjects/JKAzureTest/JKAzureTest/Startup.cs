864e743264f9e41c7093e69652846d426c275301
josephkiran <josephkiran@gmail.com>
2015-05-05 13:02:39 +0530

    Start Commit

---
+++ GitProjects/JKAzureTest/JKAzureTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JKAzureTest.Startup))]
+namespace JKAzureTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
